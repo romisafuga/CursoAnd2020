99987e584746818d5822e7c95e536021a297ded8
romisafuga <68354928+romisafuga@users.noreply.github.com>
2020-11-24 00:17:39 -0600

    Curso desde android 2020 capitulo 12-20

---
+++ app/src/main/java/com/example/cursoand2020/AnotherKotlinActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cursoand2020
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AnotherKotlinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_another_kotlin)
+    }
+}
--- app/src/main/java/com/example/cursoand2020/MainActivity.java
+++ app/src/main/java/com/example/cursoand2020/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.cursoand2020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tx1;
+    EditText ed1;
+    Button bot1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tx1 = findViewById(R.id.textView_uno);
+        tx1.setText("Hola Mundo desde el código");
+        ed1 = findViewById(R.id.editTextTextPersonName);
+        bot1 = findViewById(R.id.button);
+
+        bot1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // para comentariar una linea se usa esto
+                /* para comentariar mas de una linea se empieza
+                con esta combinacion y se termina al final del
+                comentario con la combinacion de abajo
+                 */
+                tx1.setText(ed1.getText().toString());
+                Toast.makeText(getApplicationContext(),
+                        tx1.getText().toString(),
+                        Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
